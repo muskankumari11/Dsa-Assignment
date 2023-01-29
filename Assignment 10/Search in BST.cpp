4f53821e50571bc43b63b87a223c838e336c80d5
muskankumari11 <111870054+muskankumari11@users.noreply.github.com>
2023-01-29 23:21:22 +0530

    Create Search in BST.cpp

---
+++ Assignment 10/Search in BST.cpp
@@ -0,0 +1,10 @@
+//Leetcode
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        while(root!=NULL && root->val!=val){
+            root=val<root->val? root->left:root->right;
+            }
+            return root;
+            }
+            };
