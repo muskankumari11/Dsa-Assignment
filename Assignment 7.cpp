f962ba21ec88092fa04b9bf70dbb892e4289e1ed
muskankumari11 <111870054+muskankumari11@users.noreply.github.com>
2023-01-31 09:13:13 +0530

    Create Assignment 7.cpp

---
+++ Assignment 7.cpp
@@ -0,0 +1,28 @@
+//Leeccode
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x==0){
+            return 0;
+        }
+        int s = 1 , e=x,m;
+        while(s<=e){
+            m=s+(e-s)/2;
+            if(m == x/m){
+                break;
+            }
+            else if(m > x/m){
+                e=m-1;
+            }
+            else{
+                s=m+1;
+            }
+        }
+        if(m>x/m){
+            return m-1;
+        }
+        else{
+            return m;
+        }
+    }
+};
