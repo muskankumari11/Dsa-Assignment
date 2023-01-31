a85a9d1a21bcea17b90d9cf6710330c380a83974
muskankumari11 <111870054+muskankumari11@users.noreply.github.com>
2023-01-31 11:44:48 +0530

    Create Coin arrange.cpp

---
+++ Assignment 7/Coin arrange.cpp
@@ -0,0 +1,13 @@
+//Leetcode
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        int i=1, res=0;
+        while(n>=i){
+            n=n-i;
+            res++;
+            i++;
+        }
+        return res;
+    }
+};
