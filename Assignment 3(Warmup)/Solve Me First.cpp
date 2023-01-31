ab81db4f9204426127e7165bfeed35edbca7266b
muskankumari11 <111870054+muskankumari11@users.noreply.github.com>
2023-01-31 12:03:51 +0530

    Create Solve Me First.cpp

---
+++ Assignment 3(Warmup)/Solve Me First.cpp
@@ -0,0 +1,14 @@
+//https://www.hackerrank.com/challenges/solve-me-first/problem?isFullScreen=true
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main(){
+    int a,b;
+    cin>>a>>b;
+    int sum=0;
+    sum+=a+b;
+    cout<<sum;
+    }
