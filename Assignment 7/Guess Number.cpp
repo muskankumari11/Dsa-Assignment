ec64a6de8c5eb59bd8289bc40469df048131a021
muskankumari11 <111870054+muskankumari11@users.noreply.github.com>
2023-01-31 11:42:50 +0530

    Create Guess Number.cpp

---
+++ Assignment 7/Guess Number.cpp
@@ -0,0 +1,20 @@
+//Leetcode
+class solution{
+public:
+    int guessNumber(int n) {
+        int s=1,e=n;
+        while(true){
+            int mid=s+(e-s)/2;
+            int g=guess(mid);
+            if(g==0){
+                return mid;
+            }
+            else if(g==-1){
+                e=mid-1;
+            }
+            else if(g==1){
+                s=mid+1;
+            }
+        }
+    }
+};
