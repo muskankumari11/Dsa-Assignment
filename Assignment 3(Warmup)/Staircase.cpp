83f3e031155a62c32066d40e6bc4ca538277a414
muskankumari11 <111870054+muskankumari11@users.noreply.github.com>
2023-02-11 22:09:17 +0530

    Create Staircase.cpp

---
+++ Assignment 3(Warmup)/Staircase.cpp
@@ -0,0 +1,20 @@
+https://www.hackerrank.com/challenges/staircase/problem?isFullScreen=true
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            cout << " ";
+        }
+        for (int k = 0; k <= i; k++) {
+            cout << "#";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
